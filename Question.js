98d98e71f51f530670d6bbc9e043c1648f48f80c
bht151020022023-commits <bht151020022023@gmail.com>
2026-03-26 22:14:24 +0700

    Update Question.js

--- Question.js
+++ Question.js
@@ -1,1 +1,587 @@
+let allQuestions = [
 
+{
+    id: 1,
+    question: "A register of Vietnamese airlines operating on an AOC is maintained by the.",
+    options: [
+        "A. ICAO.",
+        "B. CAAV.",
+        "C. FAA."
+    ],
+    answer: "B",
+    explanation: "CAAV quản lý và duy trì danh sách các hãng hàng không tại Việt Nam."
+},
+
+{
+    id: 2,
+    question: "What category of aircraft are required to be painted or affixed with nationality markings of Vietnam?",
+    options: [
+        "A. All public transport aircraft above 2300Kg.",
+        "B. All public transport aircraft above 5700Kg.",
+        "C. All aircraft registered in Vietnam."
+    ],
+    answer: "C",
+    explanation: "Tất cả aircraft đăng ký tại Việt Nam phải có nationality markings."
+},
+
+{
+    id: 3,
+    question: "To facilitate a repair, a drawing can be taken from.",
+    options: [
+        "A. the local authority.",
+        "B. the manufacturer.",
+        "C. the approved design organization."
+    ],
+    answer: "C",
+    explanation: "Repair phải dựa trên tài liệu từ tổ chức thiết kế được phê duyệt."
+},
+
+{
+    id: 4,
+    question: "Documents to be carried on a specific aircraft for a specific purpose is given in.",
+    options: [
+        "A. VAR Part 5",
+        "B. VAR Part 10",
+        "C. AOC"
+    ],
+    answer: "B",
+    explanation: "VAR Part 10 quy định tài liệu cần mang theo trên aircraft."
+},
+
+{
+    id: 5,
+    question: "Safe operation of the aircraft is the responsibility of the.",
+    options: [
+        "A. VAR Part 5 approved organisation.",
+        "B. CAAV.",
+        "C. Operator."
+    ],
+    answer: "C",
+    explanation: "Operator chịu trách nhiệm vận hành an toàn aircraft."
+},
+
+{
+    id: 6,
+    question: "Which set of letters and numbers below can be the marks of Vietnam nationality aircraft?",
+    options: [
+        "A. VN A221, VN B221, VN C221, VN D221, VN E221.",
+        "B. VN A221, VN B221, VN C221, VN E221, VN F221.",
+        "C. VN A221, VN B221, VN C221, VN D221."
+    ],
+    answer: "C",
+    explanation: "Marks hợp lệ gồm VN A/B/C/D + số."
+},
+
+{
+    id: 7,
+    question: "How many times must the nationality and registration marks appear on fixed-wing aircraft?",
+    options: [
+        "A. 4 times.",
+        "B. 3 times.",
+        "C. 5 times."
+    ],
+    answer: "A",
+    explanation: "Marks phải xuất hiện 4 vị trí trên aircraft."
+},
+
+{
+    id: 8,
+    question: "Aviation safety Advisory Circulars (Advisory Circulars)",
+    options: [
+        "A. are explained in more detail by the VAR Parts.",
+        "B. explain in more detail subparts of the VAR Parts.",
+        "C. provide specific technical safety requirements in support the Vietnam Civil Aviation Law and supporting legislation."
+    ],
+    answer: "C",
+    explanation: "AC cung cấp hướng dẫn kỹ thuật hỗ trợ luật hàng không."
+},
+
+{
+    id: 9,
+    question: "For aircraft in commercial air transport, how many radio communications equipment is required?",
+    options: [
+        "A. at least 01 equipment.",
+        "B. at least 02 independent equipment.",
+        "C. at least 02 main equipment and 01 backup."
+    ],
+    answer: "B",
+    explanation: "Yêu cầu tối thiểu 2 thiết bị liên lạc độc lập."
+},
+
+{
+    id: 10,
+    question: "Details of placards and markings on aircraft will be found in.",
+    options: [
+        "A. ATA Chapter 11 of the AMM.",
+        "B. Operations Manual.",
+        "C. Approved Maintenance Manual."
+    ],
+    answer: "A",
+    explanation: "ATA 11 quy định placards và markings."
+},
+{
+    id: 11,
+    question: "How long is the Maintenance Review required for aircraft with a maximum take-off weight of less than 2,730 kg?",
+    options: [
+        "A. 12 months.",
+        "B. 6 months.",
+        "C. 4 months."
+    ],
+    answer: "A",
+    explanation: "Maintenance Review cho aircraft <2730kg là 12 tháng."
+},
+
+{
+    id: 12,
+    question: "If a defect is raised and deferred.",
+    options: [
+        "A. It is recorded in Technical Manual",
+        "B. It is attached to or aligned with the aircraft technical log",
+        "C. It is recorded in Deferred Defect Manual"
+    ],
+    answer: "B",
+    explanation: "Defect deferred phải được ghi và theo dõi trong technical log."
+},
+
+{
+    id: 13,
+    question: "For CAAV registered aircraft, when maintenance is not carried out by an approved maintenance organization, who shall the maintenance release be completed and signed?",
+    options: [
+        "A. a person appropriately licensed in accordance with VAR Part 7",
+        "B. a person appropriately licensed in accordance with regulatory requirements of State of Registry other than Vietnam that complied with Annex 1",
+        "C. a person appropriately trained in accordance with VAR Part 9"
+    ],
+    answer: "A",
+    explanation: "Phải do người có license theo VAR Part 7 ký CRS."
+},
+
+{
+    id: 14,
+    question: "The responsibility to keep and maintain aircraft logbooks belongs to",
+    options: [
+        "A. the owner.",
+        "B. the operator.",
+        "C. the VAR Part145 organisation maintaining the aircraft."
+    ],
+    answer: "B",
+    explanation: "Operator chịu trách nhiệm quản lý logbook."
+},
+
+{
+    id: 15,
+    question: "As required in VAR Part 12, what logbooks (records of maintenance, check, inspections, repair, and modification activities performed on the aircraft) are required for an AOC holder?",
+    options: [
+        "A. Technical logbook.",
+        "B. Aircraft logbook.",
+        "C. Aircraft/Engine/APU/Modification Logbooks"
+    ],
+    answer: "C",
+    explanation: "Phải có đầy đủ logbook cho aircraft, engine, APU và modification."
+},
+
+{
+    id: 16,
+    question: "AOC holder’s maintenance control manual",
+    options: [
+        "A. provides how to maintain a component on an aircraft.",
+        "B. provides the use and guidance of maintenance and operational personnel concerned",
+        "C. provides when to overhaul a component."
+    ],
+    answer: "B",
+    explanation: "MCM hướng dẫn sử dụng và quản lý cho nhân sự khai thác và bảo dưỡng."
+},
+
+{
+    id: 17,
+    question: "A major repair / major modification is carried out",
+    options: [
+        "A. without the permission of CAAV",
+        "B. without need of report to CAAV after the completion",
+        "C. in accordance with technical data approved by the CAAV"
+    ],
+    answer: "C",
+    explanation: "Major repair/mod phải theo dữ liệu kỹ thuật được CAAV phê duyệt."
+},
+
+{
+    id: 18,
+    question: "Which statement below is correct",
+    options: [
+        "A. Documents to be carried on aircraft do not include certificate of Registration, Airworthiness, Insurance and Radio License.",
+        "B. Documents to be carried on aircraft do not include C of A, Noise certificate",
+        "C. Documents to be carried on aircraft include but not limit to Certificate of Registration, C of A, Noise certificate, License of use of radio devices, Technical Log, AFM or RFM, Normal, abnormal and emergency checklists."
+    ],
+    answer: "C",
+    explanation: "Aircraft phải mang đầy đủ giấy tờ như C of A, Registration, AFM, checklist..."
+},
+
+{
+    id: 19,
+    question: "One of the contents is described in Air operator certificate, which answer is correct?",
+    options: [
+        "A. Name and location of AMO",
+        "B. The type(s) of aircraft(s) authorised for use, specific authorizations, conditions and limitations.",
+        "C. Stamped and signed by Operator"
+    ],
+    answer: "B",
+    explanation: "AOC quy định loại aircraft, điều kiện, giới hạn khai thác."
+},
+{
+    id: 20,
+    question: "For all aircraft registered in Vietnam, the document to be carried on aircraft for all operations described in which VAR’s part?",
+    options: [
+        "A. VAR Part 5.",
+        "B. VAR Part 10",
+        "C. VAR Part 21"
+    ],
+    answer: "B",
+    explanation: "VAR Part 10 quy định tài liệu phải mang theo trên aircraft."
+},
+
+{
+    id: 21,
+    question: "Flight time for aeroplane are record from.",
+    options: [
+        "A. take off to touch down.",
+        "B. first passenger on board to last passenger left.",
+        "C. first moves to taxi to finally come to rest."
+    ],
+    answer: "C",
+    explanation: "Flight time tính từ lúc aircraft bắt đầu lăn bánh đến khi dừng hẳn."
+},
+
+{
+    id: 22,
+    question: "To operate an aircraft in commercial air transport in Vietnam, operators must:",
+    options: [
+        "A. hold an AOC approved by CAAV.",
+        "B. hold a DOA Certificate.",
+        "C. nonspecific requirement."
+    ],
+    answer: "A",
+    explanation: "Phải có AOC do CAAV cấp."
+},
+
+{
+    id: 23,
+    question: "Where to find information as record of all servicing of fuel and oil, defects on aircraft during operation.",
+    options: [
+        "A. Technical logbook.",
+        "B. Flight manual.",
+        "C. Aircraft Logbook."
+    ],
+    answer: "A",
+    explanation: "Technical log ghi nhận fuel, oil và defect trong khai thác."
+},
+
+{
+    id: 24,
+    question: "Amendments to VAR Parts are by way of.",
+    options: [
+        "A. orange pages.",
+        "B. grey pages.",
+        "C. red letters."
+    ],
+    answer: "C",
+    explanation: "Amendments thường được thể hiện bằng chữ đỏ."
+},
+
+{
+    id: 25,
+    question: "Deferred defects are recorded in.",
+    options: [
+        "A. the Deferred defect manual.",
+        "B. the Technical Log.",
+        "C. the Technical Manual."
+    ],
+    answer: "B",
+    explanation: "Deferred defect được ghi trong Technical Log."
+},
+
+{
+    id: 26,
+    question: "Which of the following mandatory documents must be carried on an aircraft when flying within territory of Vietnam?",
+    options: [
+        "A. Certificate of Registration.",
+        "B. Maintenance Manual",
+        "C. Engine Logbook."
+    ],
+    answer: "A",
+    explanation: "Certificate of Registration là bắt buộc."
+},
+
+{
+    id: 27,
+    question: "The Aircraft Maintenance Schedules are produced by.",
+    options: [
+        "A. manufacturer with CAAV approval.",
+        "B. operator.",
+        "C. CAAV Safety Regulation Group."
+    ],
+    answer: "B",
+    explanation: "Operator chịu trách nhiệm xây dựng maintenance schedule."
+},
+
+{
+    id: 28,
+    question: "What operations do the requirements in VAR Part 17 apply to?",
+    options: [
+        "A. Military aircraft operations",
+        "B. General aviation operations by light aircraft",
+        "C. Commercial air transport operations and General aviation operations, by Turbojet airplane and large airplane"
+    ],
+    answer: "C",
+    explanation: "VAR Part 17 áp dụng cho commercial + GA với aircraft lớn/turbojet."
+},
+
+{
+    id: 29,
+    question: "VAR Part 17 covers.",
+    options: [
+        "A. maintenance.",
+        "B. instruments and equipment.",
+        "C. mass, balance and performance"
+    ],
+    answer: "C",
+    explanation: "VAR Part 17 liên quan mass, balance và performance."
+},
+{
+    id: 30,
+    question: "VAR Part 21 Subpart G applies to.",
+    options: [
+        "A. Manufacture approval",
+        "B. Maintenance Organization approval",
+        "C. Air Operator approval"
+    ],
+    answer: "A",
+    explanation: "Subpart G liên quan đến phê chuẩn sản xuất (manufacturing)."
+},
+
+{
+    id: 31,
+    question: "Which of the following information is not a part of the aircraft Technical Log?",
+    options: [
+        "A. Fuel and oil uplift column.",
+        "B. List of approved maintenance organization contracted for aircraft maintenance AOC holder.",
+        "C. Airport entry column including the departure and arrival airport."
+    ],
+    answer: "B",
+    explanation: "Danh sách AMO không nằm trong Technical Log."
+},
+
+{
+    id: 32,
+    question: "Requirement about registration marks can be found in.",
+    options: [
+        "A. VAR Part 2.",
+        "B. VAR Part 4.",
+        "C. AC-02-001."
+    ],
+    answer: "A",
+    explanation: "VAR Part 2 quy định về registration marks."
+},
+
+{
+    id: 33,
+    question: "Documents to be carried on an aircraft include?",
+    options: [
+        "A. Tech log, full Operations Manual and Flight Manual.",
+        "B. Tech Log and parts of the Operations Manual.",
+        "C. Technical Log, C of R, C of A, Noise certificate, License of use of radio devices, AFM or RFM, Normal, abnormal and emergency checklists."
+    ],
+    answer: "C",
+    explanation: "Phải mang đầy đủ các giấy tờ và tài liệu vận hành."
+},
+
+{
+    id: 34,
+    question: "Who is responsible for approving contents, layout of aircraft Technical Log?",
+    options: [
+        "A. The AMO.",
+        "B. The Operator.",
+        "C. the CAAV"
+    ],
+    answer: "C",
+    explanation: "CAAV phê duyệt nội dung và layout Technical Log."
+},
+
+{
+    id: 35,
+    question: "The mandatory documents to be carried on an aircraft are stated in the.",
+    options: [
+        "A. VAR Part 21",
+        "B. VAR Part 5",
+        "C. VAR Part 10"
+    ],
+    answer: "C",
+    explanation: "VAR Part 10 quy định tài liệu bắt buộc."
+},
+
+{
+    id: 36,
+    question: "By CAAV requirement, the passengers carrying operations by air are unacceptable conduct, what behavior is not acceptable?",
+    options: [
+        "A. may smoke while the no-smoking sign is lighted.",
+        "B. may smoke if the flight crews accept.",
+        "C. may not smoke while the no-smoking sign is lighted and in any aircraft lavatory."
+    ],
+    answer: "C",
+    explanation: "Cấm hút thuốc khi đèn no-smoking bật và trong lavatory."
+},
+
+{
+    id: 37,
+    question: "Which VAR’s Part requires Vietnam registered aircraft to be maintained in accordance with an Approved Aircraft Maintenance Programme?",
+    options: [
+        "A. VAR Part 21.",
+        "B. VAR Part 12.",
+        "C. VAR Part 7."
+    ],
+    answer: "B",
+    explanation: "VAR Part 12 quy định chương trình bảo dưỡng."
+},
+
+{
+    id: 38,
+    question: "As described in VAR Part 12: Aircraft maintenance programme. How to build a maintenance programme?",
+    options: [
+        "A. Based on maintenance programme information made by AMO and any additional applicable experience",
+        "B. Based on maintenance programme information made by EASA, FAA.",
+        "C. Based on maintenance programme information made by the State of Design or by the organization responsible for the type design and any additional applicable experience."
+    ],
+    answer: "C",
+    explanation: "Phải dựa trên dữ liệu từ State of Design + kinh nghiệm bổ sung."
+},
+
+{
+    id: 39,
+    question: "How to check the due date of Certificate of Maintenance Review?",
+    options: [
+        "A. In the maintenance schedule.",
+        "B. On the CMR.",
+        "C. In the maintenance statement."
+    ],
+    answer: "B",
+    explanation: "Kiểm tra trực tiếp trên CMR."
+},
+{
+    id: 40,
+    question: "An operator adds a new aircraft to its fleet.",
+    options: [
+        "A. An amendment to its Approved Aircraft Maintenance Schedule (AMS) must be applied for, only if the aircraft differs in any way to the aircraft already in its fleet.",
+        "B. An amendment to its Approved Aircraft Maintenance Schedule must be applied for, only if the aircraft is a totally different Type to any of the aircraft already in its fleet.",
+        "C. An amendment to its Approved Aircraft Maintenance Schedule must be applied for, even if the aircraft is identical to the aircraft already in its fleet."
+    ],
+    answer: "C",
+    explanation: "Bất kỳ aircraft mới nào cũng cần amend AMS."
+},
+
+{
+    id: 41,
+    question: "Scheduled maintenance can be extended by authority written into the.",
+    options: [
+        "A. Approved Aircraft Maintenance Schedule.",
+        "B. Aircraft Maintenance Manual.",
+        "C. Approved Maintenance Document."
+    ],
+    answer: "A",
+    explanation: "Quyền extend nằm trong AMS được phê duyệt."
+},
+
+{
+    id: 42,
+    question: "Which aircraft type of the aircraft with the registration number VN-Bxxx?",
+    options: [
+        "A. Aircraft with jet engines (Turbofan/ Turbojet).",
+        "B. aircraft with turboprop engines (Turboprop).",
+        "C. aircraft with piston engines."
+    ],
+    answer: "B",
+    explanation: "VN-Bxxx là aircraft turboprop."
+},
+
+{
+    id: 43,
+    question: "What would be a modification?",
+    options: [
+        "A. A change has not previously been approved by FAA.",
+        "B. A change has previously been approved.",
+        "C. A change to the type design of an aircraft, engine or propeller."
+    ],
+    answer: "C",
+    explanation: "Modification là thay đổi type design."
+},
+
+{
+    id: 44,
+    question: "A licenced engineer who is convicted to a drug or alcohol-related offense may be denied any license for a period of up to:",
+    options: [
+        "A. 06 months.",
+        "B. 12 months.",
+        "C. 24 months."
+    ],
+    answer: "B",
+    explanation: "Có thể bị cấm tối đa 12 tháng."
+},
+
+{
+    id: 45,
+    question: "Certificate of Aircraft Registration of an aircraft for international flight on transport aircraft.",
+    options: [
+        "A. must be carried aboard.",
+        "B. must not be flown with aircraft.",
+        "C. does not have to be carried aboard."
+    ],
+    answer: "A",
+    explanation: "Phải mang theo trên aircraft."
+},
+
+{
+    id: 46,
+    question: "What does MEL stand for?",
+    options: [
+        "A. Manufacturer Equipment List.",
+        "B. Minimum Equipment List.",
+        "C. Maximum Equipment List."
+    ],
+    answer: "B",
+    explanation: "MEL = Minimum Equipment List."
+},
+
+{
+    id: 47,
+    question: "An aircraft operator’s MEL.",
+    options: [
+        "A. can be more restrictive than the MMEL.",
+        "B. can be less restrictive than the MMEL.",
+        "C. can be more restrictive with certain limitations."
+    ],
+    answer: "A",
+    explanation: "MEL của operator có thể nghiêm ngặt hơn MMEL."
+},
+
+{
+    id: 48,
+    question: "An Airworthiness Directive contains.",
+    options: [
+        "A. a list of maintenance tasks to be carried out on a particular aircraft as approved by the International Civil Aviation.",
+        "B. information concerning the airworthiness of aircraft and are distributed by the CAAV.",
+        "C. a list of maintenance tasks to be carried out on a particular aircraft/ aircraft component, required to be done in order to prevent endangering the safety incidents."
+    ],
+    answer: "C",
+    explanation: "AD yêu cầu các hành động bảo dưỡng bắt buộc để đảm bảo an toàn."
+},
+
+{
+    id: 49,
+    question: "Main definitions are applicable to any VAR Part could be found in?",
+    options: [
+        "A. VAR Part 1",
+        "B. VAR Part 4",
+        "C. VAR Part 5"
+    ],
+    answer: "A",
+    explanation: "VAR Part 1 chứa các định nghĩa chung."
+},
+  
+];
